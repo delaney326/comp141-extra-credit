99c396656ec0420a3afd6c3ceb62b99f44f02a41
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-05-02 20:41:30 +0000

    second commit

--- main.sh
+++ main.sh
@@ -1,1 +1,43 @@
-#!bash 
+#!/bin/bash
+#
+#script for extra credit 
+#
+mkdir output
+#
+cp ./textfile ./output
+#
+cd output
+#
+touch read.txt
+#
+mv textfile read.txt
+#
+pwd > pwd.txt
+#
+ls > ls.txt
+#
+touch copy.txt
+#
+mv textfile copy.txt
+#
+alias now= $(date)
+#
+now > date.txt
+#
+wc textfile > textcount.txt
+#
+ps -fp 1,2,3,4,5 > process.txt
+#
+ifconfig -s > netstat.txt
+#
+mount > mount.txt
+#
+touch permissions.txt 
+#
+chmod a+rwx permissions.txt
+#
+TESTENV1="test"
+#
+grep "^[A-Za-z]" textfile
+#
+cd ..
